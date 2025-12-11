76d6969fe9458567906bd97f5a75a0705c973ebe
Tu Nombre <tu-correo@loquesea.com>
2025-12-11 19:01:20 -0300

    Show Flow tickets on success page

--- apps/web/src/CompraExitosaPage.tsx
+++ apps/web/src/CompraExitosaPage.tsx
@@ -1,6 +1,7 @@
 // apps/web/src/CompraExitosaPage.tsx
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from './api';
+import MisTicketsOrganizer from './MisTicketsOrganizer';
 
 type PublicOrderResponse = {
   id: string;
@@ -176,6 +177,8 @@ export default function CompraExitosaPage() {
         <h1>Compra exitosa</h1>
         <p>{renderLeftMessage()}</p>
       </section>
+      
+        <MisTicketsOrganizer />
 
       {/* Columna derecha */}
       <section className="card-right">
