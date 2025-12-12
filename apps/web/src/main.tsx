da541973b99371461cfc4fd3e5ab0031aa1b898c
Tu Nombre <tu-correo@loquesea.com>
2025-12-12 03:19:57 -0300

    Improve purchase success page with QR and PDF

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import LandingPage from './LandingPage.tsx';
 import './index.css'
 import App from './App.tsx'
 
 const path = window.location.pathname.toLowerCase();
 
+
+// /  => landing
+// todo lo demás ( /eventos, /compra-exitosa, /lo-que-sea ) => App
+const RootComponent = path === '/' ? LandingPage : App;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RootComponent />
     <App />
   </StrictMode>,
 )
